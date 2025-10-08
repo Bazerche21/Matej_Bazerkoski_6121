747f2e17465a8c14761b110f395001fcfcdff9eb
5866 <dushanlazarovski@gmail.com>
2025-10-08 19:10:34 +0200

    Commit1

--- Matej_Bazerkoski_6121/Lecture3/Exercise3/src/Main.java
+++ Matej_Bazerkoski_6121/Lecture3/Exercise3/src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(matrix[i][j] + " ");
+                System.out.println("Test");
             }
             System.out.println();
         }
